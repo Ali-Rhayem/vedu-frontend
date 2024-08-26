b801ddc6e6252707cb405cc2b0ba1b73b8708243
Alirh1 <alirhayem.1212@gmail.com>
2024-08-26 22:06:35 +0300

    added route for login page in app.js

--- vedu-frontend/src/App.js
+++ vedu-frontend/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import login from "./pages/login/login.jsx";
+
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/Login" element={<login />} />
+      </Routes>
+    </Router>
   );
 }
 
