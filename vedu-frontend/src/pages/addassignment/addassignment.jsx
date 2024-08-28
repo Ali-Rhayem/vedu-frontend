1168efc6e60d54915e10d49c635b3393e82241c1
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 22:56:26 +0300

    initialized addassignment.jsx

---
+++ vedu-frontend/src/pages/addassignment/addassignment.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./addassignment.css";
+
+function AddAssignment() {
+  return (
+    <div className="add-assignment-page">
+      <div className="Container">
+        <div className="content">
+          <div className="assignment-form">
+            <div className="form-left">
+              <h3>New Assignment</h3>
+              <form>
+                <div className="form-group">
+                  <label>Topic</label>
+                  <select>
+                    <option value="default">Select a topic</option>
+                    {/* Add other topics here */}
+                  </select>
+                </div>
+
+                <div className="form-group">
+                  <label>Title</label>
+                  <input type="text" placeholder="Enter title" />
+                </div>
+
+                <div className="form-group">
+                  <label>Description</label>
+                  <textarea placeholder="Enter description" />
+                </div>
+
+                <div className="form-group">
+                  <label>Attach files</label>
+                  <div className="file-upload">
+                    <input type="file" multiple />
+                    <button type="button" className="upload-button">
+                      Upload
+                    </button>
+                  </div>
+                  <div className="uploaded-files"></div>
+                </div>
+
+                <div className="form-actions">
+                  <button type="button" className="cancel-button">
+                    Cancel
+                  </button>
+                  <button type="submit" className="add-button">
+                    Add
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            <div className="form-right">
+              <div className="form-group">
+                <label>Due Date</label>
+                <input type="date" />
+              </div>
+
+              <div className="form-group">
+                <label>Grade</label>
+                <input type="text" placeholder="Enter grade" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddAssignment;
