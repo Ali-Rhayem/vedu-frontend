8b945dc34510635ba146d49d7a2042a5ce8aca77
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 17:35:36 +0300

    added routing for class page in app.js

--- vedu-frontend/src/App.js
+++ vedu-frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from "./pages/login/login.jsx"; 
 import Register from "./pages/register/register.jsx"
 import Home from "./pages/home/home.jsx"
+import Class from "./pages/class/class.jsx"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/class" element={<Class />} />
       </Routes>
     </Router>
   );
