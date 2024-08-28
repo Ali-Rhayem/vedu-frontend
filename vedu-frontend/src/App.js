aed517b82ab2f2bf9bdf9dc3ca8706be4aaf1644
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 15:55:15 +0300

    solved error in naming and added route for register page

--- vedu-frontend/src/App.js
+++ vedu-frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from "./pages/login/login.jsx"; 
+import Register from "./pages/register/register.jsx"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- vedu-frontend/src/pages/register/register.jsx
+++ vedu-frontend/src/pages/register/register.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Register.css";
+import "./register.css";
 
 function Register() {
 
