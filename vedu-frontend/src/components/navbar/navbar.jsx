af626ed43e66c70c060cb383294e6cd1564c534c
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 16:52:01 +0300

    initialized navbar

---
+++ vedu-frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+
+  return (
+    <header className="top-bar">
+      <div className="search-bar">
+        <input type="text" placeholder="Search here" />
+      </div>
+      <div className="user-profile">
+        <div className="profile-info">
+          <span>Ali Rhayem</span>
+          <span>alirhayem@gmail.com</span>
+        </div>
+        <div className="profile-dropdown">
+          <ul>
+            <li>
+              <a href="/profile">Profile</a>
+            </li>
+            <li>
+              <a href="/settings">Settings</a>
+            </li>
+            <li>Logout</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
