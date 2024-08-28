e1c3c46e698fdff71e509b9a0ea754a4903984ef
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 17:08:17 +0300

    added route for navbar in app.js and solved path error in home.jsx

--- vedu-frontend/src/App.js
+++ vedu-frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from "./pages/login/login.jsx"; 
 import Register from "./pages/register/register.jsx"
+import Home from "./pages/home/home.jsx"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- vedu-frontend/src/pages/home/home.jsx
+++ vedu-frontend/src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./Home.css";
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
+import "./home.css";
+import Navbar from "../../components/navbar/navbar";
+import Sidebar from "../../components/sidebar/sidebar";
 
 function Home() {
   return (
