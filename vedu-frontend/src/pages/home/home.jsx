5734aef0f7158bba26689ba7d0be021fe30ae084
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 17:06:04 +0300

    added navbar component to home.jsx

--- vedu-frontend/src/pages/home/home.jsx
+++ vedu-frontend/src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
 
+import Navbar from "./Navbar";
 function Home() {
   return (
     <div className="home-page">
       <div className="container">
+        <Navbar />
         <div className="content">
           <div className="actions">
             <button className="join-button">Join</button>
