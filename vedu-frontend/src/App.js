fd6a3011f66d7c2bd5ff0272c4e209c6aa37539d
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 18:09:16 +0300

    added route for editprofile page in app.js

--- vedu-frontend/src/App.js
+++ vedu-frontend/src/App.js
@@ -5,6 +5,7 @@ import Register from "./pages/register/register.jsx"
 import Home from "./pages/home/home.jsx"
 import Class from "./pages/class/class.jsx"
 import Profile from './pages/profile/profile.jsx';
+import EditProfile from './pages/editprofile/editprofile.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/class" element={<Class />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
     </Router>
   );
