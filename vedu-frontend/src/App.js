829f8a562a9ac5e88635812b30223d6da10b0c17
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 23:03:16 +0300

    added route for addassignment in app.js

--- vedu-frontend/src/App.js
+++ vedu-frontend/src/App.js
@@ -7,6 +7,7 @@ import Class from "./pages/class/class.jsx"
 import Profile from './pages/profile/profile.jsx';
 import EditProfile from './pages/editprofile/editprofile.jsx';
 import EditPersonalInfo from './pages/editpersonalinfo/editpersonalinfo.jsx';
+import AddAssignment from './pages/addassignment/addassignment.jsx';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/edit-personal-info" element={<EditPersonalInfo />} />
+        <Route path="/add-assignment" element={<AddAssignment />} />
       </Routes>
     </Router>
   );
