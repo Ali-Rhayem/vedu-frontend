32564f08749c9e8107a38b70284f883710573c1f
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 17:45:26 +0300

    initialized profile.jsx

---
+++ vedu-frontend/src/pages/profile/profile.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Profile.css";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+function Profile() {
+  return (
+    <div className="profile-page">
+      <Sidebar/>
+
+      <div className="Container">
+        <Navbar />
+        <div className="content">
+          <div className="profile-content">
+            <h3>Profile</h3>
+
+            <div className="profile-section">
+              <div className="section-header">
+                <h4>My Profile</h4>
+                <button className="edit-button">
+                  Edit
+                </button>
+              </div>
+              <div className="profile-info">
+                <div className="profile-image">
+                  <img src="path/to/profile-image.png" alt="Profile" />
+                </div>
+                <div className="profile-name">
+                  <p>Name: Ali Rhayem</p>
+                  <p>Bio: Some text about Ali</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="profile-section">
+              <div className="section-header">
+                <h4>Personal Information</h4>
+                <button
+                  className="edit-button"
+                >
+                  Edit
+                </button>
+              </div>
+              <div className="personal-info">
+                <div className="info-group">
+                  <p>First Name: Ali</p>
+                  <p>Last Name: Rhayem</p>
+                </div>
+                <div className="info-group">
+                  <p>Email: alirhayem@gmail.com</p>
+                  <p>Phone Number: 123456789</p>
+                </div>
+                <div className="info-group">
+                  <p>Bio: Text</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="profile-section">
+              <div className="section-header">
+                <h4>Address</h4>
+                <button className="edit-button">
+                  Edit
+                </button>
+              </div>
+              <div className="address-info">
+                <div className="info-group">
+                  <p>Country: Text</p>
+                  <p>City: Text</p>
+                </div>
+                <div className="info-group">
+                  <p>Code: 0000</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
