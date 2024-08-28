a85974d542c364ec2867b77b57aa161dcabdf9ab
Alirh1 <alirhayem.1212@gmail.com>
2024-08-28 22:36:41 +0300

    initialized editpersonalinfo.jsx

---
+++ vedu-frontend/src/pages/editpersonalinfo/editpersonalinfo.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./editpersonalinfo.css";
+
+function EditPersonalInfo() {
+
+  return (
+    <div className="edit-personal-info-page">
+      <div className="Container">
+        <div className="content">
+          <div className="profile-content">
+            <h3>Edit Profile</h3>
+
+            <div className="profile-section">
+              <div className="profile-picture">
+                <div className="profile-img">
+                  <img src="path/to/profile-image.png" alt="Profile" />
+                </div>
+                <button className="change-photo-button">Change Photo</button>
+              </div>
+
+              <div className="profile-info">
+                <div className="info-group">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    placeholder="Example"
+                  />
+                </div>
+              </div>
+              <div className="form-actions">
+                <button className="cancel-button">
+                  Cancel
+                </button>
+                <button className="confirm-button">
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditPersonalInfo;
