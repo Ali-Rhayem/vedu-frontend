53d17ec1cefd450451794ea6e269fdd33a7d6cdf
Alirh1 <alirhayem.1212@gmail.com>
2024-08-29 00:21:49 +0300

    modified styling and added responsivness in editpersonalinfo page

--- vedu-frontend/src/pages/editpersonalinfo/editpersonalinfo.jsx
+++ vedu-frontend/src/pages/editpersonalinfo/editpersonalinfo.jsx
@@ -4,17 +4,17 @@ import Navbar from "../../components/navbar/navbar";
 import Sidebar from "../../components/sidebar/sidebar";
 
 function EditPersonalInfo() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [bio, setBio] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [bio, setBio] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   return (
     <div className="edit-profile-page">
-      <Sidebar/>
+      <Sidebar />
       <div className="Container">
         <Navbar />
         <div className="content">
@@ -26,79 +26,83 @@ function EditPersonalInfo() {
                 <h4>Personal Information</h4>
               </div>
               <div className="personal-info">
-                <div className="info-group">
-                  <label>First Name</label>
-                  <input
-                    type="text"
-                    placeholder="Example"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                  />
+                <div className="flex-row">
+                  <div className="info-group">
+                    <label>First Name</label>
+                    <input
+                      type="text"
+                      placeholder="Example"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                  </div>
+                  <div className="info-group">
+                    <label>Last Name</label>
+                    <input
+                      type="text"
+                      placeholder="Example"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  </div>
                 </div>
-                <div className="info-group">
-                  <label>Last Name</label>
-                  <input
-                    type="text"
-                    placeholder="Example"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                  />
+                <div className="flex-row">
+                  <div className="info-group">
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      placeholder="Example@gmail.com"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="info-group">
+                    <label>Bio</label>
+                    <input
+                      type="text"
+                      placeholder="Example"
+                      value={bio}
+                      onChange={(e) => setBio(e.target.value)}
+                    />
+                  </div>
                 </div>
-                <div className="info-group">
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    placeholder="Example@gmail.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
+                <div className="flex-row">
+                  <div className="info-group">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      placeholder="***********"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="info-group">
+                    <label>Confirm Password</label>
+                    <input
+                      type="password"
+                      placeholder="***********"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </div>
                 </div>
-                <div className="info-group">
-                  <label>Bio</label>
-                  <input
-                    type="text"
-                    placeholder="Example"
-                    value={bio}
-                    onChange={(e) => setBio(e.target.value)}
-                  />
-                </div>
-                <div className="info-group">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    placeholder="***********"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className="info-group">
-                  <label>Confirm Password</label>
-                  <input
-                    type="password"
-                    placeholder="***********"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                </div>
-                <div className="info-group">
-                  <label>Phone Number</label>
-                  <input
-                    type="text"
-                    placeholder="123456789"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                  />
+                <div className="flex-row">
+                  <div className="info-group">
+                    <label>Phone Number</label>
+                    <input
+                      type="text"
+                      placeholder="123456789"
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                  </div>
+                  <div className="info-group"></div>
                 </div>
               </div>
             </div>
-
             <div className="form-actions">
-              <button className="cancel-button">
-                Cancel
-              </button>
-              <button className="confirm-button">
-                Confirm
-              </button>
+              <button className="cancel-button">Cancel</button>
+              <button className="confirm-button">Confirm</button>
             </div>
           </div>
         </div>
