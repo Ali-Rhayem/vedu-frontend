8b27800571460705d82ec4daf27195f97b0619d5
Alirh1 <alirhayem.1212@gmail.com>
2024-08-30 02:45:37 +0300

    added tabs component to classpeople page

--- vedu-frontend/src/pages/classpeople/classpeople.jsx
+++ vedu-frontend/src/pages/classpeople/classpeople.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./classpeople.css";
 import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/navbar";
+import Tabs from "../../components/Tabs/tabs";
 
 function ClassPeople() {
   return (
@@ -10,6 +11,7 @@ function ClassPeople() {
       <div className="people-page-container">
         <Navbar />
         <div className="content">
+          <Tabs />
           <div className="people-content">
             <h3>Teachers</h3>
             <div className="people-section teachers">
