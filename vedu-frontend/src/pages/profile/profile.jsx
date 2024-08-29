dae705e33abc87e661a6d22f4a3354b07ac6eaef
Alirh1 <alirhayem.1212@gmail.com>
2024-08-30 01:04:23 +0300

    added userSlice in redux

--- vedu-frontend/src/pages/profile/profile.jsx
+++ vedu-frontend/src/pages/profile/profile.jsx
@@ -1,61 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./profile.css";
 import Navbar from "../../components/navbar/navbar";
 import Sidebar from "../../components/sidebar/sidebar";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { setUser } from "../../redux/userSlice/userSlice";
 import { useNavigate } from "react-router-dom";
 
 function Profile() {
   const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [bio, setBio] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [country, setCountry] = useState("");
-  const [city, setCity] = useState("");
-  const [code, setCode] = useState("");
-  const [profileImage, setProfileImage] = useState("");
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.user.data);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get("http://127.0.0.1:8000/api/user", {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
+    if (!userData) {
+      const fetchUserData = async () => {
+        try {
+          const response = await axios.get("http://127.0.0.1:8000/api/user", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+          dispatch(setUser(response.data));
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      };
 
-        const data = response.data;
-        setName(data.name);
-        setBio(data.bio);
-        setFirstName(data.first_name);
-        setLastName(data.last_name);
-        setEmail(data.email);
-        setPhoneNumber(data.phone_number);
-        setCountry(data.country);
-        setCity(data.city);
-        setCode(data.code);
-        setProfileImage(`http://127.0.0.1:8000/${data.profile_image}`);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUserData();
-  }, []);
+      fetchUserData();
+    }
+  }, [userData, dispatch]);
 
   const handleEditProfile = () => {
     navigate("/edit-profile");
-  }
+  };
 
   const handleEditPersonalInfo = () => {
     navigate("/edit-personal-info");
-  }
+  };
 
   const handleEditAddress = () => {
     navigate("/edit-address");
+  };
+
+  if (!userData) {
+    return <div>Loading...</div>;
   }
 
   return (
@@ -71,15 +60,23 @@ function Profile() {
             <div className="profile-section">
               <div className="profile-section-header">
                 <h4>My Profile</h4>
-                <button className="profile-edit-button" onClick={handleEditProfile}>Edit</button>
+                <button
+                  className="profile-edit-button"
+                  onClick={handleEditProfile}
+                >
+                  Edit
+                </button>
               </div>
               <div className="profile-info">
                 <div className="profile-image">
-                  <img src={profileImage} alt="Profile" />
+                  <img
+                    src={`http://127.0.0.1:8000/${userData.profile_image}`}
+                    alt="Profile"
+                  />
                 </div>
                 <div className="profile-name">
-                  <p>Name: {name}</p>
-                  <p>Bio: {bio}</p>
+                  <p>Name: {userData.name}</p>
+                  <p>Bio: {userData.bio}</p>
                 </div>
               </div>
             </div>
@@ -87,15 +84,19 @@ function Profile() {
             <div className="profile-section">
               <div className="profile-section-header">
                 <h4>Personal Information</h4>
-                <button className="profile-edit-button" onClick={handleEditPersonalInfo}>Edit</button>
+                <button
+                  className="profile-edit-button"
+                  onClick={handleEditPersonalInfo}
+                >
+                  Edit
+                </button>
               </div>
               <div className="profile-personal-info">
                 <div className="profile-info-group profile-flex-row">
-                  <p>Email: {email}</p>
-                  <p className="profile-phone">Phone Number: {phoneNumber}</p>
-                </div>
-                <div className="profile-info-group">
-                  <p>Bio: {bio}</p>
+                  <p>Email: {userData.email}</p>
+                  <p className="profile-phone">
+                    Phone Number: {userData.phone_number}
+                  </p>
                 </div>
               </div>
             </div>
@@ -103,15 +104,20 @@ function Profile() {
             <div className="profile-section">
               <div className="profile-section-header">
                 <h4>Address</h4>
-                <button className="profile-edit-button" onClick={handleEditAddress}>Edit</button>
+                <button
+                  className="profile-edit-button"
+                  onClick={handleEditAddress}
+                >
+                  Edit
+                </button>
               </div>
               <div className="profile-address-info">
                 <div className="profile-info-group profile-flex-row">
-                  <p>Country: {country}</p>
-                  <p className="profile-city">City: {city}</p>
+                  <p>Country: {userData.country}</p>
+                  <p className="profile-city">City: {userData.city}</p>
                 </div>
                 <div className="profile-info-group">
-                  <p>Code: {code}</p>
+                  <p>Code: {userData.code}</p>
                 </div>
               </div>
             </div>
--- vedu-frontend/src/redux/store.js
+++ vedu-frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice/authSlice';
+import userReducer from './userSlice/userSlice';  
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 });
 
