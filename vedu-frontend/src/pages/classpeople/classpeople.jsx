3e220a3919cb96c8fcb0b2ab0c2c876d1122ec7a
Alirh1 <alirhayem.1212@gmail.com>
2024-08-30 02:43:47 +0300

    initialized classpeople page

---
+++ vedu-frontend/src/pages/classpeople/classpeople.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./classpeople.css";
+
+function ClassPeople() {
+  return (
+    <div className="people-page">
+      <div className="people-page-container">
+        <div className="content">
+          <div className="people-content">
+            <h3>Teachers</h3>
+            <div className="people-section teachers">
+              <div className="person-item">
+                <div className="person-icon"></div>
+                <div className="person-details">
+                  <span>Ali Rhayem</span>
+                </div>
+              </div>
+              <div className="person-item">
+                <div className="person-icon"></div>
+                <div className="person-details">
+                  <span>Ali Rhayem</span>
+                </div>
+              </div>
+            </div>
+
+            <h3>Students</h3>
+            <div className="people-section students">
+              <div className="person-item">
+                <div className="person-icon"></div>
+                <div className="person-details">
+                  <span>Ali Rhayem</span>
+                </div>
+              </div>
+              <div className="person-item">
+                <div className="person-icon"></div>
+                <div className="person-details">
+                  <span>Ali Rhayem</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClassPeople;
