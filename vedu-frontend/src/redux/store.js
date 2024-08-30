74777a7c63f2fc13549fb9277f31b0d059b74a08
Alirh1 <alirhayem.1212@gmail.com>
2024-08-30 23:45:17 +0300

    added coursesreducer to store.js

--- vedu-frontend/src/redux/store.js
+++ vedu-frontend/src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice/authSlice';
-import userReducer from './userSlice/userSlice';  
+import userReducer from './userSlice/userSlice';
+import coursesReducer from "./coursesSlice/coursesSlice";
+
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    courses: coursesReducer,
   },
 });
 
