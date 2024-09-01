4b104160e25a61aa0a1ac5d7a12cb66b8ddba265
Alirh1 <alirhayem.1212@gmail.com>
2024-09-01 17:04:35 +0300

    added assignment slice to store

--- vedu-frontend/src/redux/store.js
+++ vedu-frontend/src/redux/store.js
@@ -2,13 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice/authSlice';
 import userReducer from './userSlice/userSlice';
 import coursesReducer from "./coursesSlice/coursesSlice";
-
+import assignmentsReducer from "./assignmentsSlice/assignmentsSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     courses: coursesReducer,
+    assignments: assignmentsReducer,
   },
 });
 
