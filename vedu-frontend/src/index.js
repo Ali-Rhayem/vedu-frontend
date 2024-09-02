647fb5e48899ca4cf324d603cd97e94b8d79ddc1
Alirh1 <alirhayem.1212@gmail.com>
2024-09-02 22:11:42 +0300

    added redux persist to index.js

--- vedu-frontend/src/index.js
+++ vedu-frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
+    </PersistGate>
+  </Provider>
 );
 
 reportWebVitals();
