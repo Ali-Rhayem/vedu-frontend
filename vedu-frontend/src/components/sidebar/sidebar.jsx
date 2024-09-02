65e7aaba4a405c1a0a4eca6c4399f1193a1612b2
Alirh1 <alirhayem.1212@gmail.com>
2024-09-02 22:07:55 +0300

    removed fetch courses manually and fetch them from redux persist courses

--- vedu-frontend/src/components/sidebar/sidebar.jsx
+++ vedu-frontend/src/components/sidebar/sidebar.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import "./sidebar.css";
-import { requestApi } from "../../utils/request";
-import { RequestMethods } from "../../utils/request_methods";
-import { setCourses } from "../../redux/coursesSlice/coursesSlice";
 
 const Sidebar = () => {
-  const dispatch = useDispatch();
   const courses = useSelector((state) => state.courses.courses);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchCourses = async () => {
-      if (courses.length === 0) {
-        try {
-          const data = await requestApi({
-            route: "/api/user/courses",
-            requestMethod: RequestMethods.GET,
-            navigationFunction: navigate,
-          });
-
-          dispatch(setCourses(data.student_courses.concat(data.instructor_courses)));
-        } catch (error) {
-          console.error("Error fetching courses:", error);
-        }
-      }
-    };
-
-    fetchCourses();
-  }, [dispatch, navigate, courses]);
 
   const handleCourseClick = (courseId) => {
     navigate(`/class/${courseId}`);
