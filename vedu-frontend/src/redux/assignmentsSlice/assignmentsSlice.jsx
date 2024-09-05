7e2429d467fe49d399ee3b4251712fe166e659e1
Alirh1 <alirhayem.1212@gmail.com>
2024-09-05 17:48:41 +0300

    updated assignment slice to handle adding assignments and topics

--- vedu-frontend/src/redux/assignmentsSlice/assignmentsSlice.jsx
+++ vedu-frontend/src/redux/assignmentsSlice/assignmentsSlice.jsx
@@ -1,25 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  assignments: [],
-};
+const initialState = {};
 
 const assignmentsSlice = createSlice({
   name: "assignments",
   initialState,
   reducers: {
-    setAssignments(state, action) {
-      state.assignments = action.payload;
+    setAssignments: (state, action) => {
+      const { classId, topics } = action.payload;
+      state[classId] = topics;
     },
-    addAssignment(state, action) {
-      state.assignments.push(action.payload);
+    addAssignment: (state, action) => {
+      const { classId, topicName, assignment } = action.payload;
+      if (state[classId]) {
+        if (!state[classId][topicName]) {
+          state[classId][topicName] = [];
+        }
+        state[classId][topicName].push(assignment);
+      }
     },
-    resetAssignments: (state) => {
-      state.assignments = [];
+    removeAssignment: (state, action) => {
+      const { classId, topicName, assignmentId } = action.payload;
+      if (state[classId] && state[classId][topicName]) {
+        state[classId][topicName] = state[classId][topicName].filter(
+          (assignment) => assignment.id !== assignmentId
+        );
+      }
+    },
+    clearAssignments: (state, action) => {
+      const { classId } = action.payload;
+      delete state[classId];
     },
   },
 });
 
-export const { setAssignments, addAssignment,resetAssignments } = assignmentsSlice.actions;
+export const { setAssignments, addAssignment, removeAssignment, clearAssignments } =
+  assignmentsSlice.actions;
 
 export default assignmentsSlice.reducer;
