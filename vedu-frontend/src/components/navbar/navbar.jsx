80c7ca9c6412ba838e8d0eeaf407b6e50527c945
Alirh1 <alirhayem.1212@gmail.com>
2024-09-08 22:54:02 +0300

    updated navbar to display user photo and clear redux local storage on logout

--- vedu-frontend/src/components/navbar/navbar.jsx
+++ vedu-frontend/src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,14 +6,30 @@ import { resetCourses } from "../../redux/coursesSlice/coursesSlice";
 import { clearUser, setUser } from "../../redux/userSlice/userSlice";
 import { requestApi } from "../../utils/request";
 import { RequestMethods } from "../../utils/request_methods";
-import persistStore from "redux-persist/es/persistStore";
-import store from "../../redux/store";
+import { clearAllAssignments } from "../../redux/assignmentsSlice/assignmentsSlice";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const userData = useSelector((state) => state.user.data);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleClickOutside = (event) => {
+    if (!event.target.closest(".user-profile")) {
+      setDropdownVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   useEffect(() => {
     if (!userData) {
@@ -53,19 +69,22 @@ const Navbar = () => {
 
   const handleLogout = () => {
     // localStorage.removeItem("token");
-  
+
     // const persistor = persistStore(store);
-    // persistor.pause();  
-  
+    // persistor.pause();
+
     // persistor.flush().then(() => {
-    //   return persistor.purge(); 
+    //   return persistor.purge();
     // }).then(() => {
     //   dispatch(resetCourses());
     //   dispatch(clearUser());
     //   console.log(userData);
-  
+
     // });
     localStorage.clear();
+    dispatch(resetCourses());
+    dispatch(clearUser());
+    dispatch(clearAllAssignments());
     navigate("/login");
   };
 
@@ -74,28 +93,41 @@ const Navbar = () => {
       <div className="search-bar">
         <input type="text" placeholder="Search here" />
       </div>
-      <div className="user-profile">
+      <div className="user-profile" onClick={toggleDropdown}>
         <div className="profile-info">
-          <span>{userData.name}</span>
-          <span>{userData.email}</span>
+          <div className="name-email">
+            <span>{userData.name}</span>
+            <span>{userData.email}</span>
+          </div>
+          <img
+            src={
+              userData?.profile_image
+                ? `http://127.0.0.1:8000/${userData.profile_image}`
+                : "/assets/images/defaultpfp.jpg"
+            }
+            alt="User Profile"
+            className="profile-image"
+          />
         </div>
-        <div className="profile-dropdown">
-          <ul>
-            <li>
-              <a onClick={handleProfile}>Profile</a>
-            </li>
-            <li>
-              <a onClick={handleEditAddress}>address</a>
-            </li>
-            <li>
-              <a onClick={handleEditPersonalInfo}>edit personal info</a>
-            </li>
-            <li>
-              <a onClick={handleEditProfile}>edit profile</a>
-            </li>
-            <li onClick={handleLogout}>Logout</li>
-          </ul>
-        </div>
+        {dropdownVisible && (
+          <div className="profile-dropdown">
+            <ul>
+              <li onClick={handleProfile}>
+                <a>Profile</a>
+              </li>
+              <li onClick={handleEditAddress}>
+                <a>Address</a>
+              </li>
+              <li onClick={handleEditPersonalInfo}>
+                <a>Edit Personal Info</a>
+              </li>
+              <li onClick={handleEditProfile}>
+                <a>Edit Profile</a>
+              </li>
+              <li onClick={handleLogout}>Logout</li>
+            </ul>
+          </div>
+        )}
       </div>
     </header>
   );
