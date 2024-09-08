6fe24510bc112927eb006af629308a251d0acf6f
Alirh1 <alirhayem.1212@gmail.com>
2024-09-08 22:56:59 +0300

    updated addpersonmodal to handle and display errors

--- vedu-frontend/src/pages/addpersonmodal/addpersonmodal.jsx
+++ vedu-frontend/src/pages/addpersonmodal/addpersonmodal.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import "./addpersonmodal.css";
 
-function AddPersonModal({ type, onClose, onSubmit }) {
+function AddPersonModal({ type, onClose, onSubmit, error, setModalError }) {
   const [email, setEmail] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setModalError(""); 
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,29 +18,23 @@ function AddPersonModal({ type, onClose, onSubmit }) {
     <div className="addperson-overlay">
       <div className="addperson-content">
         <h3>Add {type} by Email</h3>
+        {error && <p className="error-message-addpersonmodal">{error}</p>}
         <form onSubmit={handleSubmit}>
           <label>
             {type} Email:
             <input
               type="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleEmailChange}
               required
               placeholder="Enter email address"
             />
           </label>
           <div className="button-container">
-            <button
-              type="button"
-              className="close-button"
-              onClick={onClose}
-            >
+            <button type="button" className="close-button" onClick={onClose}>
               Cancel
             </button>
-            <button
-              type="submit"
-              className="action-button"
-            >
+            <button type="submit" className="action-button">
               Add {type}
             </button>
           </div>
