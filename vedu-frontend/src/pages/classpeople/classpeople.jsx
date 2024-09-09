42565ff658df92c40139ac08b516320118923fad
Alirh1 <alirhayem.1212@gmail.com>
2024-09-09 15:10:50 +0300

    updated classpeople removed functions and added them to useclasspeople

--- vedu-frontend/src/pages/classpeople/classpeople.jsx
+++ vedu-frontend/src/pages/classpeople/classpeople.jsx
@@ -1,182 +1,28 @@
-import React, { useEffect, useState, useCallback, useDebugValue } from "react";
+import React from "react";
 import "./classpeople.css";
 import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/navbar";
 import Tabs from "../../components/Tabs/tabs";
-import { useParams } from "react-router-dom";
-import { requestApi } from "../../utils/request";
-import { RequestMethods } from "../../utils/request_methods";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "../../redux/userSlice/userSlice";
 import AddPersonModal from "../addpersonmodal/addpersonmodal";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
-import {
-  setClassPeopleLoading,
-  setClassPeopleSuccess,
-  setClassPeopleError,
-  addInstructor,
-  addStudent,
-} from "../../redux/classPeopleSlice";
+import { useParams } from "react-router-dom";
+import { useClassPeople } from "./useClassPeople";
 
 function ClassPeople() {
   const { classId } = useParams();
-  const [isOwner, setIsOwner] = useState(false);
-  const [modalType, setModalType] = useState(null);
-  const courses = useSelector((state) => state.courses.courses) || [];
-  const [modalError, setModalError] = useState("");
-  const current_class = courses.find(
-    (course) => course.id === parseInt(classId)
-  );
-
-  const dispatch = useDispatch();
-  const userData = useSelector((state) => state.user.data);
-  const { instructors, students, loading, error } = useSelector((state) => ({
-    instructors: state.classPeople.instructors[classId] || [],
-    students: state.classPeople.students[classId] || [],
-    loading: state.classPeople.loading,
-    error: state.classPeople.error,
-  }));
-
-  useEffect(() => {
-    if (current_class && userData && current_class.owner_id === userData.id) {
-      setIsOwner(true);
-    }
-  }, [current_class, userData]);
-
-  useEffect(() => {
-    if (!userData) {
-      const fetchUserData = async () => {
-        try {
-          const data = await requestApi({
-            route: "/api/user",
-            requestMethod: RequestMethods.GET,
-          });
-          dispatch(setUser(data));
-        } catch (error) {
-          console.error("Error fetching user data:", error);
-        }
-      };
-      fetchUserData();
-    }
-  }, [userData, dispatch]);
-
-  useEffect(() => {
-    if (!instructors.length || !students.length) {
-      const fetchClassPeople = async () => {
-        try {
-          dispatch(setClassPeopleLoading());
-          const courseData = await requestApi({
-            route: `/api/courses/${classId}`,
-            requestMethod: RequestMethods.GET,
-          });
-
-          if (userData && userData.id === courseData.course.owner_id) {
-            setIsOwner(true);
-          }
-
-          const instructorsData = await requestApi({
-            route: `/api/course-instructor/course/${classId}/instructors`,
-            requestMethod: RequestMethods.GET,
-          });
-
-          const studentsData = await requestApi({
-            route: `/api/course-student/course/${classId}/students`,
-            requestMethod: RequestMethods.GET,
-          });
-
-          dispatch(
-            setClassPeopleSuccess({
-              classId,
-              instructors: instructorsData,
-              students: studentsData,
-            })
-          );
-        } catch (err) {
-          dispatch(setClassPeopleError("Failed to fetch class people"));
-        }
-      };
-
-      fetchClassPeople();
-    }
-  }, [classId, userData, dispatch, instructors.length, students.length]);
-
-  const handleAddPerson = async (email) => {
-    try {
-      const userIdResponse = await requestApi({
-        route: "/api/get-user-id",
-        requestMethod: RequestMethods.POST,
-        body: { email },
-      });
-
-      if (!userIdResponse || !userIdResponse.user_id) {
-        setModalError("User not found");
-        return;
-      }
-
-      const userId = userIdResponse.user_id;
-
-      const route =
-        modalType === "Instructor"
-          ? "/api/course-instructor"
-          : "/api/course-student";
-      const body = {
-        course_id: classId,
-        [`${modalType.toLowerCase()}_id`]: userId,
-      };
-
-      await requestApi({
-        route,
-        requestMethod: RequestMethods.POST,
-        body,
-      });
-
-      const instructorsData = await requestApi({
-        route: `/api/course-instructor/course/${classId}/instructors`,
-        requestMethod: RequestMethods.GET,
-      });
-
-      const studentsData = await requestApi({
-        route: `/api/course-student/course/${classId}/students`,
-        requestMethod: RequestMethods.GET,
-      });
-
-      if (modalType === "Instructor") {
-        const newInstructor = instructorsData.find(
-          (instructor) => instructor.instructor_id === userId
-        );
-        if (newInstructor) {
-          dispatch(
-            addInstructor({
-              classId,
-              instructor: newInstructor,
-            })
-          );
-        }
-      } else {
-        const newStudent = studentsData.find(
-          (student) => student.student_id === userId
-        );
-        if (newStudent) {
-          dispatch(
-            addStudent({
-              classId,
-              student: newStudent,
-            })
-          );
-        }
-      }
-
-      setModalError("");
-      setModalType(null);
-    } catch (err) {
-      if (err.status === 409) {
-        setModalError(err.response.data.message);
-      } else {
-        setModalError(err.response.data.error);
-      }
-    }
-  };
+  const {
+    isOwner,
+    modalType,
+    setModalType,
+    modalError,
+    setModalError,
+    course,
+    instructors,
+    students,
+    loading,
+    error,
+    handleAddPerson,
+  } = useClassPeople(classId);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -220,7 +66,7 @@ function ClassPeople() {
                           src={
                             instructorData.profile_image
                               ? `http://127.0.0.1:8000/${instructorData.profile_image}`
-                              : "/path-to-default-image/default-profile.png" 
+                              : "/path-to-default-image/default-profile.png"
                           }
                           alt={`${instructorData.name}'s profile`}
                           className="person-image"
@@ -280,7 +126,7 @@ function ClassPeople() {
           }}
           onSubmit={handleAddPerson}
           error={modalError}
-          setModalError={setModalError} 
+          setModalError={setModalError}
         />
       )}
     </div>
