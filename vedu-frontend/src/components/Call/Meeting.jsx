59e8d5acb9984e634cd7364637d5619d9cf310f5
Alirh1 <alirhayem.1212@gmail.com>
2024-09-11 21:43:08 +0300

    initialized meeting.jsx

---
+++ vedu-frontend/src/components/Call/Meeting.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { getUserFromLocalStorage } from '../../pages/callHelpers';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import MeetingSetup from './MeetingSetup';
+import MeetingRoom from './MeetingRoom';
+import { useGetCallById } from '../../pages/useGetCallById';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const Meeting = () => {
+  const { classId } = useParams();
+  const navigate = useNavigate();
+  const user = getUserFromLocalStorage();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  const { call, isCallLoading } = useGetCallById(classId);
+
+  useEffect(() => {
+    if (!user || !user.id) {
+      console.error("User not found or user ID is missing");
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  if (isCallLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main>
+      <StreamTheme>
+        {!isSetupComplete ? (
+          <MeetingSetup call={call} onSetupComplete={() => setIsSetupComplete(true)} />
+        ) : (
+          <StreamCall call={call}>
+            <MeetingRoom />
+          </StreamCall>
+        )}
+      </StreamTheme>
+    </main>
+  );
+};
+
+export default Meeting;
