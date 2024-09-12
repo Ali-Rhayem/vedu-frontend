d452ce71df001a3c81e6599efe26774c6a68e147
Alirh1 <alirhayem.1212@gmail.com>
2024-09-12 16:32:45 +0300

    added navigation to meeting room <> added navigation to the class page when ending call in meeting room

--- vedu-frontend/src/components/Call/MeetingRoom.jsx
+++ vedu-frontend/src/components/Call/MeetingRoom.jsx
@@ -7,11 +7,14 @@ import {
 } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 import "./MeetingRoom/MeetingRoom.css"; 
+import { useNavigate, useParams } from "react-router-dom";
 
 const MeetingRoom = () => {
   const [layout, setLayout] = useState("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const navigate = useNavigate();
+  const {classId} = useParams();
 
   const RenderCallLayout = () => {
     switch (layout) {
@@ -41,7 +44,7 @@ const MeetingRoom = () => {
       </div>
 
       <div className="call-controls-container">
-        <CallControls />
+        <CallControls onLeave={() => navigate(`/class/${classId}`)}/>
 
         <div className="dropdown-menu">
           <div className="dropdown-trigger">
@@ -77,7 +80,6 @@ const MeetingRoom = () => {
 
         <CallStatsButton />
 
-        {/* Button to toggle participants list */}
         <button
           className="participants-toggle-button"
           onClick={() => setShowParticipants((prev) => !prev)}
