e5f8d03b82caa16ed82261b05c80475ef295eabd
Alirh1 <alirhayem.1212@gmail.com>
2024-09-12 16:32:03 +0300

    added error handling in index.js <> added code to handle error in index.js

--- vedu-frontend/src/index.js
+++ vedu-frontend/src/index.js
@@ -6,6 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './redux/store';
+const originalError = console.error;
+console.error = (...args) => {
+  if (
+    typeof args[0] === "string" &&
+    args[0].includes("ResizeObserver loop limit exceeded")
+  ) {
+    return;
+  }
+  originalError(...args);
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
