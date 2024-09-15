e9c11a1a91f1c685511fde53732672a8cc0e4fdd
Alirh1 <alirhayem.1212@gmail.com>
2024-09-15 22:23:30 +0300

    modified sidebar <> updated sidebar to make it responsive

--- vedu-frontend/src/components/sidebar/sidebar.jsx
+++ vedu-frontend/src/components/sidebar/sidebar.jsx
@@ -3,40 +3,35 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ isVisible, closeSidebar }) => {
   const courses = useSelector((state) => state.courses.courses);
   const navigate = useNavigate();
-  const location = useLocation(); 
+  const location = useLocation();
 
   const handleCourseClick = (courseId) => {
+    closeSidebar();
     navigate(`/class/${courseId}`);
   };
 
   const handleHomeClick = () => {
+    closeSidebar();
     navigate("/home");
   };
 
   const isCourseActive = (courseId) => {
     return location.pathname.startsWith(`/class/${courseId}`);
-  };  
+  };
 
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${isVisible ? "visible" : ""}`}>
       <ul>
-        <li 
-          className={location.pathname === "/home" ? "active" : ""}
-          onClick={handleHomeClick}
-        >
+        <li className={location.pathname === "/home" ? "active" : ""} onClick={handleHomeClick}>
           <span className="home">Home</span>
         </li>
-        
+
         {courses.length > 0 ? (
-          courses.map((course, index) => (
-            <li 
-              key={`course-${index}-${course.id}`} 
-              onClick={() => handleCourseClick(course.id)}
-              className={isCourseActive(course.id) ? "active" : ""}
-            >
+          courses.map((course) => (
+            <li key={course.id} onClick={() => handleCourseClick(course.id)} className={isCourseActive(course.id) ? "active" : ""}>
               <span>{course.name}</span>
             </li>
           ))
