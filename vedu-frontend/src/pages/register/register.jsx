45d444292de96d4b68e5dbfc50fe22a7e4ab2e13
Alirh1 <alirhayem.1212@gmail.com>
2024-09-20 14:31:50 +0300

    updated register page <> added navbar component to register

--- vedu-frontend/src/pages/register/register.jsx
+++ vedu-frontend/src/pages/register/register.jsx
@@ -3,6 +3,7 @@ import "./register.css";
 import { useNavigate } from "react-router-dom";
 import { requestApi } from "../../utils/request";
 import { RequestMethods } from "../../utils/request_methods";
+import Navbar from "../login/Nav";
 
 function Register() {
   const navigate = useNavigate();
@@ -28,7 +29,7 @@ function Register() {
 
     try {
       const data = await requestApi({
-        includeToken: false, 
+        includeToken: false,
         route: "/api/auth/register",
         requestMethod: RequestMethods.POST,
         body: requestBody,
@@ -41,75 +42,78 @@ function Register() {
 
       console.log("Registration successful", data);
 
-      navigate("/login"); 
+      navigate("/login");
     } catch (err) {
-      setError(err.message); 
+      setError(err.message);
     }
   };
 
   return (
-    <div className="register-wrapper">
-      <div className="register-container">
-        <div className="blue-side-register">
-          {/* <div className="content-register">
+    <>
+      <Navbar />
+      <div className="register-wrapper">
+        <div className="register-container">
+          <div className="blue-side-register">
+            {/* <div className="content-register">
             <img src="path/to/your/image.png" alt="Illustration" />
             <p>lorem ipsum dolor sit amet</p>
           </div> */}
-        </div>
-        <div className="register-box">
-          <h2>Register</h2>
-          <form onSubmit={handleRegister}>
-            <div className="input-group">
-              <label>Username</label>
-              <input
-                type="text"
-                placeholder="Example"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
+          </div>
+          <div className="register-box">
+            <h2>Register</h2>
+            <form onSubmit={handleRegister}>
+              <div className="input-group">
+                <label>Username</label>
+                <input
+                  type="text"
+                  placeholder="Example"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  placeholder="Example@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  placeholder="***********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-group">
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  placeholder="***********"
+                  value={passwordConfirmation}
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                  required
+                />
+              </div>
+              {error && <p className="error-message">{error}</p>}
+              <button type="submit" className="register-button">
+                Register
+              </button>
+            </form>
+            <div className="login-link">
+              <a onClick={handleLogin}>Already have an account? Login</a>
             </div>
-            <div className="input-group">
-              <label>Email</label>
-              <input
-                type="email"
-                placeholder="Example@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-group">
-              <label>Password</label>
-              <input
-                type="password"
-                placeholder="***********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-group">
-              <label>Confirm Password</label>
-              <input
-                type="password"
-                placeholder="***********"
-                value={passwordConfirmation}
-                onChange={(e) => setPasswordConfirmation(e.target.value)}
-                required
-              />
-            </div>
-            {error && <p className="error-message">{error}</p>}
-            <button type="submit" className="register-button">
-              Register
-            </button>
-          </form>
-          <div className="login-link">
-            <a onClick={handleLogin}>Already have an account? Login</a>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
