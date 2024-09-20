7f022b1b85eb98ec2fb19b4ba53b18366a997864
Alirh1 <alirhayem.1212@gmail.com>
2024-09-20 14:30:40 +0300

    updated login page <> added navbar to the page

--- vedu-frontend/src/pages/login/login.jsx
+++ vedu-frontend/src/pages/login/login.jsx
@@ -5,6 +5,7 @@ import "./login.css";
 import { requestApi } from "../../utils/request";
 import { RequestMethods } from "../../utils/request_methods";
 import { setUser } from "../../redux/userSlice/userSlice";
+import Navbar from "./Nav";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -21,7 +22,7 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await requestApi({
         includeToken: false,
@@ -33,20 +34,20 @@ function Login() {
         },
         headers: {
           "Content-Type": "application/json",
-          "Accept": "application/json",
+          Accept: "application/json",
         },
         navigationFunction: navigate,
       });
-  
+
       localStorage.setItem("token", response.access_token);
-  
+
       if (response && response.user) {
         dispatch(setUser(response.user));
       }
 
-      localStorage.setItem('stream_token', response.stream_token);
-      localStorage.setItem('user', JSON.stringify(response.user));
-  
+      localStorage.setItem("stream_token", response.stream_token);
+      localStorage.setItem("user", JSON.stringify(response.user));
+
       navigate("/home");
     } catch (err) {
       if (err.status === 401) {
@@ -56,51 +57,61 @@ function Login() {
       }
     }
   };
-  
 
   const handleRegister = () => {
     navigate("/register");
   };
 
   return (
-    <div className="login-wrapper">
-      <div className="login-container">
-        <div className="login-box">
-          <h2>Welcome</h2>
-          <p>Login into your account</p>
-          <form onSubmit={handleLogin}>
-            <div className="input-group">
-              <label>Email</label>
-              <input
-                type="email"
-                placeholder="Example@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="input-group">
-              <label>Password</label>
-              <input
-                type="password"
-                placeholder="***********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            {error && <p className="error-message">{error}</p>}
-            {/* <div className="forgot-password">
+    <>
+      <Navbar />
+      <div className="login-wrapper">
+        <div className="login-container">
+          <div className="login-box">
+            <h2>Welcome</h2>
+            <p>Login into your account</p>
+            <form onSubmit={handleLogin}>
+              <div className="input-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  placeholder="Example@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="input-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  placeholder="***********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              {error && <p className="error-message">{error}</p>}
+              {/* <div className="forgot-password">
               <a href="#">Forgot your password?</a>
             </div> */}
-            <button type="submit" className="login-button">Log in</button>
-            <button type="button" className="google-button" onClick={handleRegister}>Create Account</button>
-          </form>
-          {/* <div className="create-account">
+              <button type="submit" className="login-button">
+                Log in
+              </button>
+              <button
+                type="button"
+                className="google-button"
+                onClick={handleRegister}
+              >
+                Create Account
+              </button>
+            </form>
+            {/* <div className="create-account">
             <a onClick={handleRegister}>Create account ?</a>
           </div> */}
+          </div>
+          <div className="blue-side"></div>
         </div>
-        <div className="blue-side"></div>
       </div>
-    </div>
+    </>
   );
 }
 
