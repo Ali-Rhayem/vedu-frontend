d087893fe031048d47b8de3e0deb687a0544880b
Alirh1 <alirhayem.1212@gmail.com>
2024-09-20 14:21:09 +0300

    updated output.jsx page <> replaced alerts with toasts

--- vedu-frontend/src/components/Call/Compiler/Output.jsx
+++ vedu-frontend/src/components/Call/Compiler/Output.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import { executeCode } from "../../../api";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Output = ({ editorRef, language, output, onOutputUpdate,HasEditAccess }) => {
+const Output = ({
+  editorRef,
+  language,
+  output,
+  onOutputUpdate,
+  HasEditAccess,
+}) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
   const runCode = async () => {
     if (!editorRef.current) {
-      alert("Editor is not yet ready");
+      toast.error("Editor is not yet ready", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
       return;
     }
 
@@ -19,10 +34,32 @@ const Output = ({ editorRef, language, output, onOutputUpdate,HasEditAccess }) =
       const { run: result } = await executeCode(language, sourceCode);
       const outputResult = result.output.split("\n");
       onOutputUpdate(outputResult);
-      result.stderr ? setIsError(true) : setIsError(false);
+      if (result.stderr) {
+        setIsError(true);
+        toast.error("There was an error while executing the code", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+      } else {
+        setIsError(false);
+      }
     } catch (error) {
       console.error(error);
-      alert(`An error occurred: ${error.message || "Unable to run code"}`);
+      toast.error(
+        `An error occurred: ${error.message || "Unable to run code"}`,
+        {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        }
+      );
     } finally {
       setIsLoading(false);
     }
@@ -43,6 +80,7 @@ const Output = ({ editorRef, language, output, onOutputUpdate,HasEditAccess }) =
           ? output.map((line, i) => <p key={i}>{line}</p>)
           : 'Click "Run Code" to see the output here'}
       </div>
+      <ToastContainer />
     </div>
   );
 };
